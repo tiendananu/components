ee0c08f2c9e4b62d7726f75134bc2cd598288fe6
Juan Badano <jb@target-solutions.net>
2020-10-01 23:30:06 +0300

    removed config

--- src/passportButton.js
+++ src/passportButton.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import IconButton from '@material-ui/core/IconButton'
 import Link from '@material-ui/core/Link'
-import config from './config'
 
 const images = {
   google:
@@ -10,12 +9,12 @@ const images = {
     'https://res.cloudinary.com/cloud-polarbear/image/upload/v1601576616/facebook.svg'
 }
 
-export default ({ integration }) => {
+export default ({ integration, secureUrl }) => {
   return (
     <Link
       tabIndex={32}
       style={{ margin: 'auto 0' }}
-      href={`${config.getModuleUrl('secure')}/auth/${integration}`}
+      href={`${secureUrl}/auth/${integration}`}
     >
       <IconButton tabIndex={32}>
         <img width="18" height="18" src={images[integration]} />
