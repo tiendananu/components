273441d6a6a60642516ad5f676f3d2956c73e5b0
Juan Badano <jb@target-solutions.net>
2020-10-10 20:19:47 +0300

    language styles

--- src/language.js
+++ src/language.js
@@ -1,29 +1,33 @@
 import React from 'react'
 import Select from '@material-ui/core/Select'
 
-export default ({ i18n }) => (
-  <Select
-    native
-    color="secondary"
-    style={{ border: 'none' }}
-    IconComponent={'div'}
-    value={i18n.language}
-    onChange={(e) => {
-      const language = e.target.value
-      if (i18n.language != language) i18n.changeLanguage(language)
-    }}
-    inputProps={{
-      style: { padding: '6px 3px 2px 7px', border: 'none' },
-      name: 'language',
-      id: 'language'
-    }}
-  >
-    {(i18n.options.whitelist || [])
-      .filter((language) => language !== 'cimode')
-      .map((language) => (
-        <option key={language} value={language}>
-          {language.toUpperCase()}
-        </option>
-      ))}
-  </Select>
-)
+export default ({ i18n, inputProps, ...props }) =>
+  i18n ? (
+    <Select
+      native
+      color="secondary"
+      IconComponent={'div'}
+      value={i18n.language}
+      onChange={(e) => {
+        const language = e.target.value
+        if (i18n.language != language) i18n.changeLanguage(language)
+      }}
+      inputProps={{
+        style: { padding: '6px 3px 2px 7px', border: 'none' },
+        name: 'language',
+        id: 'language',
+        ...inputProps
+      }}
+      {...props}
+    >
+      {(i18n.options.whitelist || [])
+        .filter((language) => language !== 'cimode')
+        .map((language) => (
+          <option key={language} value={language}>
+            {language.toUpperCase()}
+          </option>
+        ))}
+    </Select>
+  ) : (
+    ''
+  )
