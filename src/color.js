69298275e49dbe32a9cbbe087c1bded954a07318
Juan Badano <jb@target-solutions.net>
2020-12-05 16:17:23 +0200

    color

--- src/color.js
+++ src/color.js
@@ -1,46 +1,43 @@
 import React from 'react'
 import get from 'lodash/get'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
-import Avatar from '@material-ui/core/Avatar'
+import Box from '@material-ui/core/Box'
 import Fade from '@material-ui/core/Fade'
 import { ChromePicker } from 'react-color'
 
 export default ({ id, values, children, handleChange }) => {
   const [open, setOpen] = React.useState(false)
   return (
-    <FormControlLabel
-      style={{ width: '100%', margin: 0 }}
-      labelPlacement="top"
-      control={
+    <Box
+      display="flex"
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
+    >
+      <Box mx={2} display="flex" alignItems="center">
+        <Typography>{children}</Typography>
+      </Box>
+      <div
+        style={{
+          background: get(values, id),
+          height: 40,
+          width: 40,
+          borderRadius: 300
+        }}
+      />
+      <Fade in={open}>
         <div
-          onMouseEnter={() => setOpen(true)}
-          onMouseLeave={() => setOpen(false)}
+          style={{
+            position: 'absolute',
+            marginLeft: '-62px',
+            zIndex: 9999
+          }}
         >
-          <Avatar
-            style={{
-              background: get(values, id),
-              margin: '15px auto'
-            }}
+          <ChromePicker
+            disableAlpha
+            color={get(values, id)}
+            onChange={({ hex }) => handleChange({ target: { id, value: hex } })}
           />
-          <Fade in={open}>
-            <div
-              style={{
-                position: 'absolute',
-                marginLeft: '-62px',
-                zIndex: 9999
-              }}
-            >
-              <ChromePicker
-                color={get(values, id)}
-                onChange={({ hex }) =>
-                  handleChange({ target: { id, value: hex } })
-                }
-              />
-            </div>
-          </Fade>
         </div>
-      }
-      label={children}
-    />
+      </Fade>
+    </Box>
   )
 }
