8a75d20d7c1d1c1c2212143618d0f4e6c5ccf9d1
Juan Badano <jb@target-solutions.net>
2020-12-13 18:11:32 +0200

    color border

--- src/color.js
+++ src/color.js
@@ -22,8 +22,8 @@ export default ({ id, values, children, handleChange, style }) => {
             background: get(values, id),
             height: 40,
             width: 40,
-            borderColor: '#ddd',
-            borderRadius: 300
+            borderRadius: 300,
+            border: '3px solid #ddd'
           }
         }
       />
